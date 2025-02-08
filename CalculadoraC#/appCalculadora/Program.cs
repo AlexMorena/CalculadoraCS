e1ff7da74467d6dac880c7de927b2696a31bd612
Alejandro Morena Rodríguez <156430677+AlexMorena@users.noreply.github.com>
2025-02-08 19:19:16 +0100

    Add files via upload
    Miniprograma de una calculadora desarrollado en el lenguaje C#

---
+++ CalculadoraC#/appCalculadora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Calculadora en C# ===");
+            Console.WriteLine("Operaciones disponibles: +, -, *, /");
+            Console.Write("Ingresa el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingresa el operador (+, -, *, /): ");
+            char operador = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.Write("Ingresa el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+            bool operacionValida = true;
+
+            switch (operador)
+            {
+                case '+': resultado = num1 + num2; break;
+                case '-': resultado = num1 - num2; break;
+                case '*': resultado = num1 * num2; break;
+                case '/':
+                    if (num2 != 0) resultado = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error: No se puede dividir por cero.");
+                        operacionValida = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Operador no válido.");
+                    operacionValida = false;
+                    break;
+            }
+
+            if (operacionValida)
+            {
+                Console.WriteLine($"\nResultado: {num1} {operador} {num2} = {resultado}");
+            }
+
+            Console.Write("\n¿Deseas hacer otro cálculo? (S/N): ");
+            char respuesta = Console.ReadKey().KeyChar;
+            if (char.ToUpper(respuesta) != 'S') break;
+        }
+    }
+}
+
